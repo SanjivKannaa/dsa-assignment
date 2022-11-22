de542269c8d59024d24b5b8eab3fd4921fb53a3a
sanjiv kannaa jeganathan <jsanjiv2003@gmail.com>
2022-11-22 08:42:21 +0530

    added q1

---
+++ q1.c
@@ -0,0 +1,75 @@
+// Implement stack using linkedlist
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *next;
+};
+struct node *head = NULL;
+
+void push(int element){
+    struct node *current = head;
+    struct node *newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data = element;
+    newnode->next = NULL;
+    if (current!=NULL){
+        while (current->next!=NULL){
+            current = current->next;
+        }
+        current->next = newnode;
+    }else{
+        current = newnode;
+    }
+}
+
+void pop(){
+    if (head==NULL){
+        printf("stack underflow");
+    }else{
+        struct node *current = head;
+        while (current->next!=NULL){
+            current = current->next;
+        }
+        printf("%d", current->next->data);
+        free(current->next);
+        current=NULL;
+    }
+}
+
+void view_stack(){
+    struct node *current = head;
+    current = current->next;
+    while(current!=NULL){
+        printf("%d ", current->data);
+        current = current->next;
+    }
+    // printf("%d ", current->data);
+}
+
+int main(){
+    while(1){
+        printf("\n\nchoose operation\n1. push\n2. pop\n3. view stack\n4. exit\n\t=>");
+        int choice;
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            printf("enter the element: ");
+            int element;
+            scanf("%d", &element);
+            push(element);
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            view_stack();
+            break;
+        case 4:
+            return 0;
+        default:
+            printf("invalid option");
+        }
+    }
+}
